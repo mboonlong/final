5280dce371e496ccb55f0cee5af2ae50ad9e2036
Stacey Hoffman <stacey.hoffman@kellogg.northwestern.edu>
2021-03-14 09:32:16 -0500

    lambda updates

--- netlify/functions/get_reviews.js
+++ netlify/functions/get_reviews.js
@@ -32,7 +32,7 @@ exports.handler = async function(event) {
       Image: imageUrl,                       
       Rating: rating,
       Review: review,
-      created: firebase.firestore.FieldValue.serverTimestamp()
+      // created: firebase.firestore.FieldValue.serverTimestamp()
     })
   }
 
